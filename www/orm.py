cb85b7467f711631b737d76a82c0363b3db9802c
duchangtuan <fengxi1986@gmail.com>
2018-02-28 19:58:17 +0800

    add complete orm.py but some bugs left

--- www/orm.py
+++ www/orm.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin python3
 # -*- coding: utf-8 -*-
 
+import sys
 import asyncio, logging
 import aiomysql
 
@@ -17,7 +18,7 @@ def create_pool(loop, **kw):
             user=kw['user'],
             password=kw['password'],
             db=kw['db'],
-            charset=kw.get('charset', 'utf-8'),
+            charset=kw.get('charset', 'utf8'),
             autocommit=kw.get('autocommit', True),
             maxsize=kw.get('maxsize', 10),
             minsize=kw.get('minsize', 1),
@@ -29,7 +30,7 @@ def select(sql, args, size=None):
     log(sql, args)
     global __pool
     with (yield from __pool) as conn:
-        cur = yield from conn.cursor(aiomysql.DictCuros)
+        cur = yield from conn.cursor(aiomysql.DictCursor)
         yield from cur.execute(sql.replace('?', '%s'), args or ())
         if size:
             rs = yield from cur.fetchmany(size)
@@ -72,4 +73,163 @@ class Field(object):
         # the function used to print the object of the class elegantly
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+class StringField(Field):
 
+    def __init__(self, name=None, column_type='varchar(100)', primary_key=False, default=None):
+        super().__init__(name, column_type, primary_key, default)
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        # exclude Model class
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # get table name
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+
+        # get all the Field and primary_key
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info(' found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    # find primary_key
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # keep the mappings between attribute and column
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # primary key attribute name
+        attrs['__fields__'] = fields # attribute name that exclude primary key
+
+        # below four methods keep the default insert, delete, update and select operation.
+        # `` is used to avoid with sql keyword, or sql execution maybe wrong
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), \
+                primary_key, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, \
+                ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+def Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mapping__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else filed.default
+                logging.debug('using default value for %s: %s' %(key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    async def find(cls, pk):
+        """
+        find object by primary key.
+        """
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls__primary_key), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+        await destory_pool()
+
+    async def destory_pool():
+        global __pool
+        if __pool is not None:
+            __pool.close()
+            await __pool.wait_closed()
+
+#class User(Model):
+#    __table__ = 'users'
+#
+#    id = IntegerField('id')
+#    name = StringField('username')
+#    email = StringField('email')
+#    password = StringField('password')
+#    #id = IntegerField(primary_key=True)
+#    #name = StringField()
+#
+#def test( loop ):
+#    yield from create_pool(host='127.0.0.1', loop = loop, user='root', password='root', db='test' )
+#    u=User(id=12345,email='test22@test.com',passwd='test',name='fengxi')
+#    #u=User(id='123', name='fengxi')
+#    yield from u.save()
+#
+#if __name__ == '__main__':
+#
+#    loop = asyncio.get_event_loop()
+#    loop.run_until_complete( asyncio.wait([test( loop )]) )
+#    loop.close()
+#    if loop.is_closed():
+#        sys.exit(0)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(create_pool(host='127.0.0.1', port=3306, user='root', password='root', db='test', charset='utf8', loop=loop))
+    rs = loop.run_until_complete(select('select * from orm', None))
+
+    print('res: %s' % rs)
+    loop.close()
+
+    #if loop.is_closed():
+    #    sys.exit(0)
